242a65b19978327163a1a6442f7233678bf0c8e2
Jefferson Duarte <duartejefferson123@gmail.com>
2026-02-12 22:55:59 -0300

    feat: Criando e implementando UserRepository

---
+++ src/main/java/com/icc/tarefas/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.icc.tarefas.repository;
+
+import com.icc.tarefas.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByEmail(String email);
+}
